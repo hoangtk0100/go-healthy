bbeddafbdabd345b7e040d7ebd197ed3445e7b30
Hoang Tran <hoangtk.0100@gmail.com>
2023-04-01 18:31:04 +0700

    chore(db): Added mock user

---
+++ db/migration/000004_mock_user.up.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (
+    username,
+    email,
+    full_name,
+    phone_number,
+    gender,
+    type,
+    avatar_url,
+    hashed_password
+) VALUES (
+    'user@demo.com', 'user@demo.com', 'Demo User', '84999999999', 'MALE', 'USER', '', '$2a$10$Ac3s4gomklistGXSJWWKXuFdjAY4V.Uenrvvw.9NjNbOhsjCsHh7u'
+);
