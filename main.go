da4fa4367c141020bba5f8b120e59c453b6f777b
Hoang Tran <hoangtk.0100@gmail.com>
2023-04-03 07:24:23 +0700

    feat(db-migration): Runned migration before starting server

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	"log"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 
 	"github.com/hoangtk0100/go-healthy/api"
@@ -22,6 +25,8 @@ func main() {
 		log.Fatal("Cannot connect to DB", err)
 	}
 
+	runDBMigration(config.MigrationURL, config.DBSource)
+
 	store := db.NewStore(conn)
 	server, err := api.NewServer(config, store)
 	if err != nil {
@@ -32,4 +37,17 @@ func main() {
 	if err != nil {
 		log.Fatal("Cannot start server", err)
 	}
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("can not create new migrate instance", err)
+	}
+
+	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("failed to run migrate up")
+	}
+
+	log.Println("db migrated successfully")
 }
